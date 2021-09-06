00e55bc2d8978588e52ca8a49fa7a89ac5ab90a4
Wendy Liga <me@wendyliga.com>
2021-09-06 16:55:52 +0700

    update

--- src/Tulsi/ProjectEditorConfigManagerViewController.swift
+++ src/Tulsi/ProjectEditorConfigManagerViewController.swift
@@ -180,7 +180,7 @@ final class ProjectEditorConfigManagerViewController: NSViewController {
         NSWorkspace.shared.open(projectURL)
       }
       
-      if let newOutputPath = newOutputPath {
+      if let newOutputPath = newOutputPath, newOutputPath != self.outputPath {
         self.outputPath = newOutputPath
       }
     }
